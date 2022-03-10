e24db8d4efa83d157b8d2bfde3db25cd3cd57652
DaniPazLor <85082509+DaniPazLor@users.noreply.github.com>
2022-03-10 13:49:04 +0100

    Update AplicacionPrincipal.java

--- PSP/Tareas/Tarea 6/Tarea_6_PSP/src/actividad_6_1/AplicacionPrincipal.java
+++ PSP/Tareas/Tarea 6/Tarea_6_PSP/src/actividad_6_1/AplicacionPrincipal.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package actividad_6_1;
 
 import java.io.*;
@@ -13,8 +8,10 @@ import java.util.regex.*;
 import java.util.logging.*;
 
 /**
+ * Clase principal que contiene el método main y el resto de métodos para
+ * conformar la lógica de negocio de la aplicación
  *
- * @author Pauda
+ * @author Daniel Paz Lorenzo
  */
 public class AplicacionPrincipal {
 
@@ -22,81 +19,104 @@ public class AplicacionPrincipal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Declaración de variables
         boolean correcto;
         Scanner teclado = new Scanner(System.in);
+        //Declaración de los patrones
         final String patronLogin = "[a-z]{8}";
         final String patronNomFichero = "^[a-zA-Z0-9]{1,8}.[a-zA-Z]{3}$";//Patrón indicando cuál es el inicio el final de la cadena
 
         /*--------------------------------------------------------------------*/
         /*Establecemos un fichero de registros de la aplicación*/
         Logger logger = creaFicheroRegistro();
-
         /*--------------------------------------------------------------------*/
+        //Solicitamos login al usuario
         do {
             correcto = false;
             try {
                 System.out.println("Introduzca el nombre de usuario: ");
                 String nomUsuario = teclado.next();
 
-                if (comprobarPatron(patronLogin, nomUsuario)) {
+                if (comprobarPatron(patronLogin, nomUsuario)) {//Si patrón coincide
                     correcto = true;
                     // Añado un mensaje al log   
-                    String textoLog = "El nombre usuario " + nomUsuario + " es correcto";
+                    String textoLog = "El patron (" + patronLogin + ") del usuario " + nomUsuario + " es correcto";
                     logger.log(Level.INFO, textoLog);
-
+                    Thread.sleep(200);//Pausa de aplicacion
                 } else {
                     // Añado un mensaje al log   
-                    String textoLog = "El nombre usuario " + nomUsuario + " es INcorrecto";
+                    String textoLog = "El patron (" + patronLogin + ") del usuario " + nomUsuario + " es INcorrecto";
                     logger.log(Level.WARNING, textoLog);
+                    Thread.sleep(200);//Pausa de aplicacion
                 }
 
-            } catch (Exception e) {
+            } catch (Exception e) {//Recogemos excepción si las hubiera
                 System.out.println("Se ha producido la excepción " + e.getMessage());
+                correcto = false;
             }
 
         } while (!correcto);
 
+        //Solicitamos nombre del fichero al usuario
         do {
             correcto = false;
             try {
                 System.out.println("Introduzca el nombre del fichero: ");
                 String nomFichero = teclado.next();
 
-                if (comprobarPatron(patronNomFichero, nomFichero)) {
+                if (comprobarPatron(patronNomFichero, nomFichero)) {//Si patrón coincide
                     correcto = true;
                     // Añado un mensaje al log   
-                    String textoLog = "El nombre del fichero " + nomFichero + " es correcto";
+                    String textoLog = "El patron (" + patronNomFichero + ") del fichero " + nomFichero + " es correcto";
                     logger.log(Level.INFO, textoLog);
-                    correcto=visualizarFichero(nomFichero, logger);
+                    Thread.sleep(200);//Pausa de aplicacion
+                    correcto = visualizarFichero(nomFichero, logger);
                 } else {
                     // Añado un mensaje al log   
-                    String textoLog = "El nombre usuario " + nomFichero + " es INcorrecto";
+                    String textoLog = "El patron (" + patronNomFichero + ") del fichero " + nomFichero + " es INcorrecto";
                     logger.log(Level.WARNING, textoLog);
+                    Thread.sleep(200);//Pausa de aplicacion
                 }
 
-            } catch (Exception e) {
+            } catch (Exception e) {//Recogemos excepción si las hubiera
                 System.out.println("Se ha producido la excepción " + e.getMessage());
+                correcto = false;
             }
 
         } while (!correcto);
-        
+
     }
 
+    /**
+     * Método para comparar el patrón con el texto introducido
+     *
+     * @param patron de la clase Pattern
+     * @param texto introducido por el usuario
+     * @return resultado boolean de la comparación
+     */
     public static boolean comprobarPatron(String patron, String texto) {
         Pattern pat;
         Matcher mat;
-
+        //Compilamos y comparamos el patrón con el texto
         pat = Pattern.compile(patron);
         mat = pat.matcher(texto);
 
-        return mat.find();
+        return mat.find();//boolean true si coinciden patrón y texto
     }
 
+    /**
+     * Método que permite visualizar por pantalla el contenido de un fichero
+     * pasado por parámetro.
+     *
+     * @param nombreFichero del tipo String
+     * @param logger de la clse Logger
+     * @return boolean true/false dependiendo si existe o no el fichero
+     */
     public static boolean visualizarFichero(String nombreFichero, Logger logger) {
         File archivo;
         FileReader fr = null;
         BufferedReader br;
-        boolean existeFichero=false;
+        boolean existeFichero = false;
 
         try {
             // Apertura del fichero y creacion de BufferedReader para poder
@@ -107,25 +127,27 @@ public class AplicacionPrincipal {
 
             //Comprueba que fichero exista
             if (archivo.exists()) {
-                existeFichero=true;
+                existeFichero = true;
                 // Añado un mensaje al log   
                 String textoLog = "El fichero " + nombreFichero + " existe dentro de la carpeta C:\\datos\\";
                 logger.log(Level.INFO, textoLog);
+                Thread.sleep(200);//Pausa de aplicacion
                 // Lectura del fichero
+                System.out.println("-------------------------------------------");
+                System.out.println("- CONTENIDO DEL FICHERO " + nombreFichero + "-");
+                System.out.println("-------------------------------------------");
                 String linea;
                 while ((linea = br.readLine()) != null) {
                     System.out.println(linea);
                 }
-            } else {
-                existeFichero=false;
-                // Añado un mensaje al log   
-                String textoLog = "El fichero " + nombreFichero + " NO existe dentro de la carpeta C:\\datos\\";
-                logger.log(Level.WARNING, textoLog);
-                
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            existeFichero = false;
+            // Añado un mensaje al log   
+            String textoLog = "El fichero " + nombreFichero + " NO existe dentro de la carpeta C:\\datos\\";
+            logger.log(Level.WARNING, textoLog);
+            Thread.sleep(200);//Pausa de aplicacion
         } finally {
             // En el finally cerramos el fichero, para asegurarnos
             // que se cierra tanto si todo va bien como si salta 
@@ -134,13 +156,19 @@ public class AplicacionPrincipal {
                 if (fr != null) {
                     fr.close();
                 }
-            } catch (IOException e2) {
-                e2.printStackTrace();
+            } catch (Exception e2) {
+                System.out.println("Ocurrió una excepción " + e2.getMessage());
             }
             return existeFichero;
         }
     }
 
+    /**
+     * Método para generar un fichero de log de todos los registros que se van
+     * generando en la aplicación
+     *
+     * @return un log de la clase Logger
+     */
     public static Logger creaFicheroRegistro() {
         Logger logger = Logger.getLogger("Registro");
         try {
@@ -153,7 +181,7 @@ public class AplicacionPrincipal {
             logger.setUseParentHandlers(true);//Mostrar por pantalla
             logger.setLevel(Level.INFO);//Nivel de seguridad INFO
             logger.setLevel(Level.ALL);//Registrar todos los eventos
-
+        //Recogemos excepciones
         } catch (IOException ex) {
             Logger.getLogger(AplicacionPrincipal.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SecurityException ex) {
@@ -162,6 +190,5 @@ public class AplicacionPrincipal {
 
         return logger;
     }
-
 
 }
