1edd3b46fb599ead36e1c044ba27ac9981ecb2de
DaniPazLor <85082509+DaniPazLor@users.noreply.github.com>
2022-04-29 12:43:03 +0200

    t6 pmdm update

---
+++ PMDM/Tareas/Tarea 6/juegoRobots/dist/data/VariadoTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="PMDM06_CONT_R20_Tileset" tilewidth="16" tileheight="16" tilecount="16384" columns="128">
+ <image source="PMDM06_CONT_R20_Tileset.png" width="2048" height="2048"/>
+</tileset>
